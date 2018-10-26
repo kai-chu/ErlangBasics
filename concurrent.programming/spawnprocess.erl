ae2d0d71cc83f8d32d1a556d0d8615be7c9ec57c
Kai Chu <chuck.chu.liu@gmail.com>
2018-10-26 23:15:33 +0200

    Add comments to the codes

--- concurrent.programming/spawnprocess.erl
+++ concurrent.programming/spawnprocess.erl
@@ -1,12 +1,21 @@
 -module(spawnprocess).
 -export([start/0, say_something/2]).
 
+%
+% spawn two processes within this module and given paramenters
+% return 0
+%
 start() -> 
 	spawn(?MODULE, say_something, [hello, 3]),
-    spawn(?MODULE, say_something, [goodbye, 3]).
+    spawn(?MODULE, say_something, [goodbye, 3]),
+    0.
 
 say_something(What, 0) ->
     done;
 say_something(What, Times) ->
 	io:format("~p~n", [What]),
     say_something(What, Times - 1).
+
+
+% To compile, erlc spawnprocess.erlc
+% To Run, erl --no-shell -s spawnprocess start -s init stop
