5c2df7e19158e201633feb369d0c31f47ff4625e
Kai Chu <chuck.chu.liu@gmail.com>
2018-10-26 23:09:26 +0200

    Changed folder structure

--- concurrent.programming/spawnprocess.erl
+++ concurrent.programming/spawnprocess.erl
@@ -1,9 +1,9 @@
--module(howtoprocess).
+-module(spawnprocess).
 -export([start/0, say_something/2]).
 
 start() -> 
-	spawn(howtoprocess, say_something, [hello, 3]),
-    spawn(howtoprocess, say_something, [goodbye, 3]).
+	spawn(?MODULE, say_something, [hello, 3]),
+    spawn(?MODULE, say_something, [goodbye, 3]).
 
 say_something(What, 0) ->
     done;
